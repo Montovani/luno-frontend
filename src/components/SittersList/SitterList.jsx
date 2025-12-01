3d3f256ce9b11c4a1715a7f7c22e88cefd715805
Igor Montovani <igor.montovani@gmail.com>
2025-12-01 22:07:50 +0100

    Add Searchbox component and Started working in the Find a Sitter page

---
+++ src/components/SittersList/SitterList.jsx
@@ -0,0 +1,11 @@
+import styles from './SitterList.module.css'
+
+function SitterList() {
+  return (
+    <div className={styles.container}>
+        
+    </div>
+  )
+}
+
+export default SitterList
